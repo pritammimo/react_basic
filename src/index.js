c871fb378251892ff891a72bd26fe521bf03b7ee
pritammimo <crpritamsaha@gmail.com>
2021-12-21 13:57:34 +0000

    push

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { ApolloClient,InMemoryCache,ApolloProvider} from '@apollo/client';
+// https://rickandmortyapi.com/graphql 
+const client=new ApolloClient({
+  uri:'https://rickandmortyapi.com/graphql',
+  cache:new InMemoryCache()
+})
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
