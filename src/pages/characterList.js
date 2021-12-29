e0b14fc645eacb6b7c85f090a448f11909451c50
pritammimo <crpritamsaha@gmail.com>
2021-12-29 10:59:25 +0000

    push2

--- src/pages/characterList.js
+++ src/pages/characterList.js
@@ -1,6 +1,21 @@
 import React from 'react'
-
+import {useQuery,gql} from "@apollo/client";
+const GET_CHARACTERS=gql
+`query{
+    characters{
+        results{
+            id
+            name
+            image
+        }
+    }
+}
+`
 export default function characterList(){
+    const obj=useQuery(GET_CHARACTERS);
+    obj.error;
+    obj.loading;
+    obj.data;
     return (
         <div>
             
